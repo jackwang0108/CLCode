01f6a36e43c38ea11ea203ec177ed91a9e169860
jackwang <jack4shihong@gmail.com>
2024-07-08 02:06:35 +0800

    formatted

--- models/si.py
+++ models/si.py
@@ -1,16 +1,12 @@
-# Copyright 2020-present, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Davide Abati, Simone Calderara.
-# All rights reserved.
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 import torch
 import torch.nn as nn
 from utils.args import *
 from models.utils.continual_model import ContinualModel
 
+
 def get_parser() -> ArgumentParser:
-    parser = ArgumentParser(description='Continual Learning Through'
-                                        ' Synaptic Intelligence.')
+    parser = ArgumentParser(
+        description='Continual Learning Through Synaptic Intelligence.')
     add_management_args(parser)
     add_experiment_args(parser)
     parser.add_argument('--c', type=float, required=True,
@@ -25,6 +21,9 @@ class SI(ContinualModel):
     NAME = 'si'
     COMPATIBILITY = ['class-il', 'domain-il', 'task-il']
 
+    ARXIV = "https://arxiv.org/abs/1703.04200"
+    TITLE = "Continual Learning Through Synaptic Intelligence"
+
     def __init__(self, backbone, loss, args, transform):
         super(SI, self).__init__(backbone, loss, args, transform)
 
@@ -36,15 +35,18 @@ class SI(ContinualModel):
         if self.big_omega is None:
             return torch.tensor(0.0).to(self.device)
         else:
-            penalty = (self.big_omega * ((self.net.get_params() - self.checkpoint) ** 2)).sum()
+            penalty = (self.big_omega *
+                       ((self.net.get_params() - self.checkpoint) ** 2)).sum()
             return penalty
 
     def end_task(self, dataset):
         # big omega calculation step
         if self.big_omega is None:
-            self.big_omega = torch.zeros_like(self.net.get_params()).to(self.device)
+            self.big_omega = torch.zeros_like(
+                self.net.get_params()).to(self.device)
 
-        self.big_omega += self.small_omega / ((self.net.get_params().data - self.checkpoint) ** 2 + self.args.xi)
+        self.big_omega += self.small_omega / \
+            ((self.net.get_params().data - self.checkpoint) ** 2 + self.args.xi)
 
         # store parameters checkpoint and reset small_omega
         self.checkpoint = self.net.get_params().data.clone().to(self.device)
