198897c0ec764de2c7a84b981b1bb4ad692eedcb
jackwang <jack4shihong@gmail.com>
2024-07-11 15:53:08 +0800

    add type hint

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import datetime
 import importlib
 import urllib.request
 from argparse import ArgumentParser
+from typing import Callable, Optional
 
 # Third-Party Library
 import numpy  # needed (don't change it)
@@ -26,7 +27,7 @@ from utils.best_args import best_args
 from utils.conf import set_random_seed
 from utils.args import add_management_args
 from utils.continual_training import train as ctrain
-from utils.types import Args
+from utils.types import Args, CVBackboneImpl
 
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
@@ -47,26 +48,27 @@ def lecun_fix():
 def parse_args():
     parser = ArgumentParser(description='CL-Codes', allow_abbrev=False)
 
-    # set the model
+    # main args
     parser.add_argument('--model', type=str, required=True,
                         help='Model to train', choices=get_all_models())
-
-    # set the backbone
     parser.add_argument('--backbone', type=str, default='resnet18',
                         help='Backbone to use')
-
-    # if load the best parameters
     parser.add_argument('--load_best_args', action='store_true',
                         help='Loads the best arguments for each method, dataset and memory buffer.')
 
+    # management args
     add_management_args(parser)
 
     args: Args = parser.parse_known_args()[0]
 
     torch.set_num_threads(4)
 
+    # load model args
+    parser: ArgumentParser
+    get_parser: Callable[..., ArgumentParser]
     mod = importlib.import_module('models.' + args.model)
     if args.load_best_args:
+        # load from founded best args
         parser.add_argument('--dataset', type=str, required=True,
                             choices=DATASET_NAMES,
                             help='Which dataset to perform experiments on.')
@@ -93,6 +95,8 @@ def parse_args():
         if args.model == 'joint' and args.dataset == 'mnist-360':
             args.model = 'joint_gcl'
     else:
+
+        # load from command line
         get_parser = getattr(mod, 'get_parser')
         parser = get_parser()
         args = parser.parse_args()
@@ -104,21 +108,27 @@ def parse_args():
     return args
 
 
-def main(args=None):
+def main(args: Optional[Args] = None):
     lecun_fix()
+
+    # parse args
     if args is None:
         args = parse_args()
 
+    # if using meta-learning
     if 'meta' in args.backbone:
         from backbone.meta_layers import get_gpu_id
         get_gpu_id(args.gpu_id)
 
-    # Add uuid, timestamp and hostname for logging
+    # add uuid, timestamp and hostname for logging
     args.conf_jobnum = str(uuid.uuid4())
     args.conf_timestamp = str(datetime.datetime.now())
     args.conf_host = socket.gethostname()
+
+    # setup dataset
     dataset = get_dataset(args)
 
+    # setup training config
     if args.n_epochs is None and isinstance(dataset, ContinualDataset):
         args.n_epochs = dataset.get_epochs()
     if args.batch_size is None:
@@ -126,14 +136,20 @@ def main(args=None):
     if hasattr(importlib.import_module('models.' + args.model), 'Buffer') and not hasattr(args, 'minibatch_size'):
         args.minibatch_size = dataset.get_minibatch_size()
 
-    backbone = dataset.get_backbone(args.backbone)
-    loss = dataset.get_loss()
+    # get backbone
+    backbone: CVBackboneImpl = dataset.get_backbone(args.backbone)
+
+    # get loss
+    loss: Callable = dataset.get_loss()
+
+    # get model
     model = get_model(args, backbone, loss, dataset.get_transform())
 
     # set job name
     setproctitle.setproctitle('{}_{}_{}'.format(
         args.model, args.buffer_size if 'buffer_size' in args else 0, args.dataset))
 
+    # train
     if isinstance(dataset, ContinualDataset):
         train(model, dataset, args)
     else:
