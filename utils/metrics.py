1fbd67b10f145ac06cabd9748901842740190cba
jackwang <jack4shihong@gmail.com>
2024-07-13 00:37:50 +0800

    add typing

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,12 +1,7 @@
-# Copyright 2020-present, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Davide Abati, Simone Calderara.
-# All rights reserved.
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 import numpy as np
 
 
-def backward_transfer(results):
+def backward_transfer(results: list[float]) -> float:
     n_tasks = len(results)
     li = list()
     for i in range(n_tasks - 1):
@@ -15,7 +10,7 @@ def backward_transfer(results):
     return np.mean(li)
 
 
-def forward_transfer(results, random_results):
+def forward_transfer(results: list[float], random_results: list[float]) -> float:
     n_tasks = len(results)
     li = list()
     for i in range(1, n_tasks):
@@ -24,14 +19,14 @@ def forward_transfer(results, random_results):
     return np.mean(li)
 
 
-def forgetting(results):
+def forgetting(results: list[float]) -> float:
     n_tasks = len(results)
     li = list()
     for i in range(n_tasks - 1):
         results[i] += [0.0] * (n_tasks - len(results[i]))
     np_res = np.array(results)
-    maxx = np.max(np_res, axis=0)
+    max_res = np.max(np_res, axis=0)
     for i in range(n_tasks - 1):
-        li.append(maxx[i] - results[-1][i])
+        li.append(max_res[i] - results[-1][i])
 
     return np.mean(li)
